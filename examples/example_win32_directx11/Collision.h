6bbd632e93d44f6b6a8ecb2df4150c273895dc40
DominikAlb <dominik.albiniak@o2.pl>
2022-04-05 18:24:53 +0200

    final

--- examples/example_win32_directx11/Collision.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <examples/example_win32_directx11/Collector.h>
-#include <examples/example_win32_directx11/Ball.h>
-template<class T = float>
-class Collision {
-public:
-
-    
-};
